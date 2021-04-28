8724c0fd9afc1873117b0ef407f693f24811d215
Alejaldo <statfluct@yahoo.com>
2021-04-29 02:02:28 +0500

    Change username regex and lowercase method

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,27 +4,25 @@ class User < ApplicationRecord
   ITERATIONS = 20000
   DIGEST = OpenSSL::Digest::SHA256.new
   VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  VALID_USERNAME = /\A[a-zA-Z0-9_]*\z/
+  VALID_USERNAME = /\A(?=.*[a-zA-Z])([\w])*\z/
 
   attr_accessor :password
 
   has_many :questions
 
-  before_validation { self.username = username.downcase }
+  before_validation :lowercase
   before_save :encrypt_password 
 
-  validates :password, presence:true, on: :create, confirmation: true
+  validates :password, presence: true, on: :create, confirmation: true
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL }
-  validates :username, presence: true, uniqueness: true, length: { maximum: 40 }, format: { with: VALID_USERNAME }
+  validates :username, presence: true, uniqueness: true, allow_nil: true, length: { maximum: 40 }, format: { with: VALID_USERNAME }
 
-  private
-
-  def encrypt_password
-    if self.password.present?
-      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
-      self.password_hash = User.hash_to_string(OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST))
-    end
+  def lowercase
+    self.username = username.downcase
+    self.email = email.downcase
   end
+  
+  private
 
   def self.hash_to_string(password_hash)
     password_hash.unpack('H*')[0]
@@ -37,6 +35,13 @@ class User < ApplicationRecord
       user
     else
       nil
+    end
+  end
+
+  def encrypt_password
+    if self.password.present?
+      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+      self.password_hash = User.hash_to_string(OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST))
     end
   end
 end
