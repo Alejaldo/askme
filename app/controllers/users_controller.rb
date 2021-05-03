92e4a5e06c591b3664f66148eca836f098f9895d
Alejaldo <statfluct@yahoo.com>
2021-05-03 21:47:59 +0500

    Add sessions and smth else

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,52 +1,57 @@
 class UsersController < ApplicationController
+  before_action :load_user, except: [:index, :create, :new]
+  before_action :authorize_user, except: [:index, :new, :create, :show]
+
   def index
-    # Создаём массив из двух болванок пользователей. Вызываем метод # User.new, который создает модель, не записывая её в базу.
-    # У каждого юзера мы прописали id, чтобы сымитировать реальную
-    # ситуацию – иначе не будет работать хелпер путей
-    @users = [
-      User.new(
-        id: 1,
-        name: 'Boris',
-        username: 'bojo',
-        avatar_url: 'https://pbs.twimg.com/profile_images/1331215386633756675/NodbPVQv_400x400.jpg'
-      ),
-      User.new(
-        id: 2,
-        name: 'Barak',
-        username: 'obama'
-      )
-    ]
+    @users = User.all
   end
 
   def new
+    redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
+    @user = User.new
+  end
+
+  def create
+    redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
+    @user = User.new(user_params)
+    
+    if @user.save
+      redirect_to root_url, notice: 'Глобалист успешно зарегистрирован с системе!'
+    else
+      render 'new' 
+    end
   end
 
   def edit
+    @user = User.find params[:id]
+  end
+
+  def update
+    @user = User.find params[:id]
+
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: 'Данные глобалиста обновлены'
+    else
+      render 'edit' 
+    end
   end
 
   def show
-    @user = User.new(
-      name: 'Boris',
-      username: 'bojo',
-      avatar_url: 'https://pbs.twimg.com/profile_images/1331215386633756675/NodbPVQv_400x400.jpg'
-    )
+    @user = User.find params[:id]
+    @question = @user.questions.order(created_at: :desc)
+    @new_question = @user.questions.build
+  end
 
-    @questions = [
-      Question.new(text: 'Как жизнь?', created_at: Date.parse('11.04.2021')),
-      Question.new(text: 'Сколько $ стоит должность конгрессмена?', created_at: Date.parse('12.04.2021')),
-      Question.new(text: 'Вы действительно брали ипотеку на ремонт своих аппартаментов?', created_at: Date.parse('13.04.2021')),
-      Question.new(text: 'Ну что, когда введем цифровой концлагерь?', created_at: Date.parse('15.04.2021')),
-      Question.new(text: 'Какого быть из рода Спенсеров?', created_at: Date.parse('17.04.2021')),
-      Question.new(text: 'Готовы идти на новый срок?', created_at: Date.parse('19.04.2021')),
-      Question.new(text: 'Давали уже советы по Амерекситу?', created_at: Date.parse('20.04.2021')),
-      Question.new(text: 'Где услуги лоббистов дешевле?', created_at: Date.parse('22.04.2021')),
-      Question.new(text: 'Ждете воцарение Уильяма?', created_at: Date.parse('23.04.2021')),
-      Question.new(text: 'Вы лично ждете Даджаля в Айят-Софии?', created_at: Date.parse('25.04.2021')),
-      Question.new(text: 'Как Вам удается продвигать Румский проект?', created_at: Date.parse('27.04.2021')),
-      Question.new(text: 'Расскажите о Вашей бурной молодости времен Балингтон клаб?', created_at: Date.parse('29.04.2021')),
-      Question.new(text: 'А вы лично нарушали регламент своего же локудауна?', created_at: Date.parse('30.04.2021'))
-    ]
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :name, :username, :avatar_url)
+  end
 
-    @new_question = Question.new
+  def load_user
+    @user ||= User.find params[:id]
+  end
+
+  def authorize_user
+    reject_user unless @user == current_user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root 'users#index'
 
-  resources :users
-  resources :questions
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, except: [:destroy]
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :questions, except: [:show, :new, :index]
+
+  get 'sign_up' => 'users#new'
+  get 'log_out' => 'sessions#destroy'
+  get 'log_in' => 'sessions#new'
 end
