508a2cad2281f819b1d9bbc21b81110d40222655
Alejaldo <statfluct@yahoo.com>
2021-05-05 11:20:49 +0500

    Delete @ from session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.authenticate(params[:email], params[:password])
 
     if user.present?
-      session[:user_id] = @user.id
+      session[:user_id] = user.id
       redirect_to root_url, notice: 'Вы успешно залогинились'
     else
       flash.now.alert = 'Неправильный мэйл или пароль'
