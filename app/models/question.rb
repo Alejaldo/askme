51615b59b19bda7548a84a85553b28c7413c1483
Alejaldo <statfluct@yahoo.com>
2021-06-01 10:10:27 +0500

    Add create_hash_tags and extract_hash_tags to Question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -6,4 +6,19 @@ class Question < ApplicationRecord
   has_many :hash_tags, through: :question_hash_tags
 
   validates :text, presence: true, length: { maximum: 255 }
+
+  after_commit :create_hash_tags, on: :create
+
+  def create_hash_tags
+    extract_hash_tags.each do |name|
+      hash_tags.create(name: name)
+    end
+  end
+
+  def extract_hash_tags
+    question_parts = [text, answer].map do |question_part|
+      question_part.scan(/#[[:word:]]+/).map{ |text| text.gsub("#", "") }
+    end
+    question_parts[0] + question_parts[1]
+  end
 end
