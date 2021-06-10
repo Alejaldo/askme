1ad5e19c9219280ac97a8485b44fb45d787c8d39
Alejaldo <statfluct@yahoo.com>
2021-06-10 10:48:39 +0500

    Modify Session and User route paths

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,7 @@ Rails.application.routes.draw do
   root 'users#index'
 
   resources :users
-  resources :questions, except: [:show, :new, :index]
-  resources :hash_tag, only: [:show]
-
-  get '/sign_up', to: 'users#new'
-  get '/log_in', to: 'sessions#new'
-  get '/search', to: 'search#index'
-  post '/log_in', to: 'sessions#create'
-  delete '/log_out', to: 'sessions#destroy'
+  resources :questions, except: %i[show new index]
+  resources :hash_tag, only: %i[show]
+  resource :session, only: %i[new create destroy]
 end
