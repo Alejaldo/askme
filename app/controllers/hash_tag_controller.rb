584fe848c3820637fe4851bcc10b3a92a842117c
Alejaldo <statfluct@yahoo.com>
2021-07-29 03:30:52 +0500

    Add exception for non-existing hashtags

--- app/controllers/hash_tag_controller.rb
+++ app/controllers/hash_tag_controller.rb
@@ -1,6 +1,12 @@
 class HashTagController < ApplicationController
+  suka = rescue_from ActiveRecord::RecordNotFound do |exception|
+    @exception = params[:id]
+    render partial: "hash_tag/exception", as: @exception
+  end
+
   def show
     @hash_tag = HashTag.find_by!(name: params[:id])
     @questions = @hash_tag.questions
+    suka
   end
 end
--- app/models/hash_tag.rb
+++ app/models/hash_tag.rb
@@ -1,4 +1,4 @@
 class HashTag < ApplicationRecord
-  has_many :question_hash_tags
+  has_many :question_hash_tags, dependent: :destroy
   has_many :questions, through: :question_hash_tags
 end
