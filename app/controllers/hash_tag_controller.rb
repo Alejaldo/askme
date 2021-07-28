5751513c0218e41e21f6a0cf03f02ffa98bf51f5
Alejaldo <statfluct@yahoo.com>
2021-07-29 03:34:24 +0500

    Delete unuseful variables

--- app/controllers/hash_tag_controller.rb
+++ app/controllers/hash_tag_controller.rb
@@ -1,5 +1,5 @@
 class HashTagController < ApplicationController
-  suka = rescue_from ActiveRecord::RecordNotFound do |exception|
+  rescue_from ActiveRecord::RecordNotFound do |exception|
     @exception = params[:id]
     render partial: "hash_tag/exception", as: @exception
   end
@@ -7,6 +7,5 @@ class HashTagController < ApplicationController
   def show
     @hash_tag = HashTag.find_by!(name: params[:id])
     @questions = @hash_tag.questions
-    suka
   end
 end
