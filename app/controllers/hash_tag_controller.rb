308d96f489b4e8af39ba5c29aa61da8f7e8c12b5
Alejaldo <statfluct@yahoo.com>
2021-08-02 21:37:14 +0500

    Move rescue_from to Application Controller and without partial

--- app/controllers/hash_tag_controller.rb
+++ app/controllers/hash_tag_controller.rb
@@ -1,9 +1,4 @@
 class HashTagController < ApplicationController
-  rescue_from ActiveRecord::RecordNotFound do |exception|
-    @exception = params[:id]
-    render partial: "hash_tag/exception", as: @exception
-  end
-
   def show
     @hash_tag = HashTag.find_by!(name: params[:id])
     @questions = @hash_tag.questions
