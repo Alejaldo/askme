c8d015ea61ebd242d0b4d27201c7a313d225a80a
Alejaldo <statfluct@yahoo.com>
2021-08-06 12:26:38 +0500

    Modify HashTag searching by adding where_exists method

--- app/controllers/hash_tag_controller.rb
+++ app/controllers/hash_tag_controller.rb
@@ -1,6 +1,6 @@
 class HashTagController < ApplicationController
   def show
-    @hash_tag = HashTag.find_by!(name: params[:id])
+    @hash_tag = HashTag.where_exists(:questions).find_by!(name: params[:id])
     @questions = @hash_tag.questions
   end
 end
