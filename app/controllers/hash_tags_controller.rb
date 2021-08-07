85872741dd190fcc56d03fc83df99e4c05fc9a8e
Alejaldo <statfluct@yahoo.com>
2021-08-07 16:51:07 +0500

    Use params[:name] instead of name: params[:id]pattern

--- app/controllers/hash_tags_controller.rb
+++ app/controllers/hash_tags_controller.rb
@@ -1,6 +1,6 @@
 class HashTagsController < ApplicationController
   def show
-    @hash_tag = HashTag.where_exists(:questions).find_by!(name: params[:id])
+    @hash_tag = HashTag.where_exists(:questions).find_by!(params[:name])
     @questions = @hash_tag.questions
   end
 end
